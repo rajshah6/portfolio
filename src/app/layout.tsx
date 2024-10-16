0c81bed996de623e2b673b3f064d63c470dc6724
Raj Shah <rajnshah2020@gmail.com>
2024-10-16 02:35:22 -0400

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Providers from "@/providers/Providers";
 import { Quicksand } from "next/font/google";
 import "../styles/GlobalStyles.scss";
 import styles from "../styles/Layout/MainLayout.module.scss";
+
 const quicksand = Quicksand({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -18,6 +19,10 @@ export default function RootLayout({
       <head>
         <meta name="description" content="Portfolio Website" />
         <title>Raj Shah</title>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        {/* You can also use a PNG format like this:
+            <link rel="icon" href="/favicon.png" type="image/png" /> 
+        */}
       </head>
       <body className={quicksand.className}>
         <main className={styles.layout}>
