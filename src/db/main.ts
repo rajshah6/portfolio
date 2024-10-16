6dbb01507c6add1c0881bfde2ceda2390f13c37b
Raj Shah <rajnshah2020@gmail.com>
2024-10-15 20:24:05 -0400

    added web files

---
+++ src/db/main.ts
@@ -0,0 +1,188 @@
+import schoolsphereImg from "../../public/images/schoolsphere.png";
+import portfolioImg from "../../public/images/portfolio.png";
+import myImage from "../../public/images/waterloo.png";
+import alopeciaImg from "../../public/images/alopecia.png";
+import groceryImg from "../../public/images/grocery.png";
+import icalendifyImg from "../../public/images/icalendify.png";
+
+export const heroData = {
+  img: myImage,
+  hi: "👋 Hey there! I am",
+  name: "Raj Shah",
+  expertise: ["Full-Stack Developer", "Web Designer"],
+  about:
+    "I'm a passionate junior Software Developer, currently studying Computer Science at the University of Waterloo. I love building things that live on the internet, whether that be websites, applications, or anything in between. Let’s push boundaries and build groundbreaking experiences together!",
+  btnText: "View my resume",
+};
+
+export const aboutData = {
+  title: "About Me",
+  cards: [
+    {
+      title: "How Did I Get Here?",
+      text: "My journey began when I was 12 years old, sparked by a fascination with problem-solving and technology. From leading teams at SHAD to creating cutting-edge projects, each experience has honed my skills and deepened my passion for driving impact through code and leadership.",
+    },
+    {
+      title: "Who Am I Today?",
+      text: "Now, I’m deeply immersed in the world of technology, excited by the endless possibilities it offers to create real change. From building apps to mentoring aspiring coders, I’m constantly inspired by the impact we can make through innovation and dedication!",
+    },
+    {
+      title: "What's Next?",
+      text: "The future is about pushing boundaries. I'm excited to explore my growing interests in machine learning and cryptography while continuing to lead, learn, and inspire. There's always a new horizon to chase, and I'm ready to tackle it head-on!",
+    },
+  ],
+};
+
+export const SkillsData = {
+  title: "Skills",
+  categories: [
+    {
+      title: "Languages",
+      skills: [
+        "Java",
+        "Python",
+        "C++",
+        "TypeScript",
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "C",
+        "Racket",
+        "Scheme",
+      ],
+    },
+    {
+      title: "Tools & Technologies",
+      skills: [
+        "Git",
+        "Linux",
+        "Node.js",
+        "Next.js",
+        "D3.js",
+        "Firebase",
+        "VS Code",
+      ],
+    },
+    {
+      title: "Frameworks & Libraries",
+      skills: [
+        "React",
+        "Apache",
+        "PyTorch",
+        "Flutter",   
+        "Tkinter",
+        "PyGame",
+      ],
+    },
+  ],
+};
+
+export const contactData = {
+  title: "Let's Get in Touch!",
+  cards: [
+    {
+      title: "Want to Collaborate?",
+      text: "I am open to discussing potential job opportunities or collaborations. Whether you're looking for a developer, designer, or team leader, I'm always excited to explore new projects and partnerships!",
+    },
+    {
+      title: "Let's Connect",
+      text: "Feel free to reach out to me via email or LinkedIn. I'm always open to new connections and conversations, so don't hesitate to drop me a message. I look forward to hearing from you soon!",
+    },
+  ],
+  links: [
+    {
+      label: "Email",
+      url: "mailto:rajnshah2020@gmail.com",
+    },
+    {
+      label: "GitHub",
+      url: "https://github.com/rajshah6",
+    },
+    {
+      label: "LinkedIn",
+      url: "https://www.linkedin.com/in/rshah9/",
+    },
+    {
+      label: "Resume",
+      url: "/Docs/resume.pdf",
+    },
+  ],
+};
+
+export const ProjectsData = {
+  title: "Projects",
+  Projects: [
+    {
+      name: "iCalendify",
+      image: icalendifyImg,
+      technologies: [
+        "-",
+      ],
+      description:
+        "IN PROGRESS",
+      demoUrl: "",
+      codeUrl: "https://github.com/rajshah6/iCalendify",
+    },
+    {
+      name: "SchoolSphere",
+      image: schoolsphereImg,
+      technologies: [
+        "Java,",
+        "Apache NetBeans,",
+        "OOP,",
+        "File I/O",
+      ],
+      description:
+        "SchoolSphere is a robust database system designed to streamline the organization of students, employees, and school inventory. Built using Object-Oriented principles in Java, the system leverages polymorphism, inheritance, and encapsulation to provide a scalable and maintainable solution. Developed in Apache NetBeans, it efficiently handles data storage and retrieval using file I/O operations with .txt files for employee, student, and inventory records.",
+      demoUrl: "",
+      codeUrl: "https://github.com/rajshah6/school-sphere",
+    },
+    {
+      name: "Personal Portfolio",
+      image: portfolioImg,
+      technologies: [
+        "TypeScript,",
+        "JavaScript,",
+        "CSS,",
+        "React,",
+        "Node.js,",
+        "Firebase,",
+        "GitHub Pages",
+      ],
+      description:
+        "Hey it looks like you're already here! This portfolio website was built using TypeScript, React, and CSS. It showcases my interests, projects, and skills, designed to be responsive and user-friendly! The site is open-source and hosted on GitHub Pages.",
+      demoUrl: "https://rajshah6.github.io",
+      codeUrl: "https://github.com/rajshah6/rajshah6.github.io",
+    },
+    {
+      name: "GroceryMaster",
+      image: groceryImg,
+      technologies: [
+        "Java,",
+        "Apache NetBeans,",
+        "File I/O",
+      ],
+      description:
+        "GroceryMaster simulates a grocery store’s inventory system, enabling users to add, save, and access food items. The inventory is saved in a structured inventory.txt file, allowing for persistent and manual data access. With an intuitive Java interface, users can easily search, add, or retrieve items by SKU or name. The inventory updates dynamically upon button clicks, ensuring real-time accuracy.",
+      demoUrl: "",
+      codeUrl: "https://github.com/rajshah6/grocery-master",
+    },
+    {
+      name: "Alopecia Areata", 
+      image: alopeciaImg,
+      technologies: [
+        "HTML,", 
+        "CSS,", 
+        "JavaScript,", 
+        "React,",
+        "Firebase,",
+        "GitHub Pages",
+      ],
+      description:
+        "In a world where our hair is often seen as a reflection of our identity, the experience of Alopecia Areata can feel isolating and overwhelming. This website is dedicated to shedding light on this significant yet often misunderstood condition. I created this platform to foster awareness and understanding, driven by a deep compassion for those affected by this condition. It is my hope that by sharing knowledge and personal stories, we can cultivate a community of support and empathy.",
+
+      demoUrl: "https://rajshah6.github.io/alopecia-areata/",
+      codeUrl: "https://github.com/rajshah6/alopecia-areata",
+    },
+  ],
+};
