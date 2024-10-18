a896a8ba1a8446cd294b6154f3d2622dc3c30cae
Raj Shah <rajnshah2020@gmail.com>
2024-10-18 19:17:22 -0400

    update aa

--- src/db/main.ts
+++ src/db/main.ts
@@ -185,9 +185,8 @@ export const ProjectsData = {
         "HTML,", 
         "CSS,", 
         "JavaScript,", 
-        "React,",
-        "Firebase,",
-        "GitHub Pages",
+        "GitHub Pages,",
+        "Data Binding",
       ],
       description:
         "In a world where our hair is often seen as a reflection of our identity, the experience of Alopecia Areata can feel isolating and overwhelming. This website is dedicated to shedding light on this significant yet often misunderstood condition. I created this platform to foster awareness and understanding, driven by a deep compassion for those affected by this condition. It is my hope that by sharing knowledge and personal stories, we can cultivate a community of support and empathy.",
