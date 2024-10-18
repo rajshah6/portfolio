d3ff991f384b81fc66d32db98801f67e5d9acae9
Raj Shah <rajnshah2020@gmail.com>
2024-10-18 01:36:53 -0400

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
